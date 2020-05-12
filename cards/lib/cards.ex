d89b6fa60b2c97e6f0e1844235d93a564a2c08fc
daniel machado <danielmachadopintos@gmail.com>
2020-05-12 18:16:32 -0300

    feat(first): commit init cards

---
+++ cards/lib/cards.ex
@@ -0,0 +1,42 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearst", "Diamonds"]
+
+    # each for each resulta em um array de array: [[], []]
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    # List.flatten(cards)
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  # tem o método do erlang
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+end
